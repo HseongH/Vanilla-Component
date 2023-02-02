ec47c2ea43f4b26f43cbcdd4fcffdfc4e4af841b
HseongH <hh4518@gmail.com>
2023-02-02 09:11:56 +0900

    webpack 설정 변경 및 필요 패키지 설치

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
@@ -13,11 +12,11 @@ const config = {
   output: {
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-
     new MiniCssExtractPlugin(),
 
     // Add your plugins here
